a5195430f08d232d7bbbd88ca8ce875e5ae95c04
juzz123 <391190087@qq.com>
2017-09-25 10:01:57 +0800

    fff

--- pages/my/ad.js
+++ pages/my/ad.js
@@ -95,7 +95,7 @@ Page({
       success: function (res) {
         var tempFilePaths = res.tempFilePaths
         wx.uploadFile({
-          url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+          url: 'http://localhost/project/public/api/v1/uploads', //仅为示例，非真实的接口地址
           filePath: tempFilePaths[0],
           name: 'file',
           formData: {
--- utils/config.js
+++ utils/config.js
@@ -5,7 +5,7 @@ class Config {
   }
 }
 
-Config.restUrl = 'http://localhost/project/public/api/v1/';
+Config.restUrl = 'https://www.hnziqiao.com/project/public/api/v1/';
 Config.onPay = false;  //是否启用支付
 
 export { Config };
